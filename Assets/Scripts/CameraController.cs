1b22908858e45d0a3f102335eaad23e1cffc9061
Ruben-Armas <rubenarmas2010@gmail.com>
2023-12-04 17:09:45 +0000

    v1

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField] GameObject player;
+    [SerializeField] float xMin;
+    [SerializeField] float xMax;
+
+    void Update() {
+        // Mueve la cámara como si fuera hija del Player
+        //transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+
+        // Camara que sigue al personaje (NO en Y) y no sale del mapa
+        
+
+        transform.position = new Vector3(
+            Mathf.Clamp(player.transform.position.x, xMin, xMax),   // Clamp -> valor entre un Min y un Max
+            transform.position.y,   // La y, z se quedan igual
+            transform.position.z);
+
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     [SerializeField] float playerSpeed = 1f;
     [SerializeField] float jumpForce = 1f;
-    void Update() {
 
+    [SerializeField] float lineLength = 1f;
+    [SerializeField] float offset = 1f;
+
+    [SerializeField] bool isJumping = false;
+
+    void Update() {
         // Movimiento a la derecha
-        if (Input.GetAxisRaw("Horizontal") == 1) {
+        /*if (Input.GetAxisRaw("Horizontal") == 1) {
             GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed, GetComponent<Rigidbody2D>().velocity.y);
         }
         // Movimiento a la izquierda
@@ -18,10 +23,31 @@ public class PlayerController : MonoBehaviour {
         // Movimiento a la horizontal
         if (Input.GetAxisRaw("Horizontal") == 0) {
             GetComponent<Rigidbody2D>().velocity = new Vector2(0, GetComponent<Rigidbody2D>().velocity.y);
-        }
-        // Salto
-        if (Input.GetButtonDown("Fire1")) {
+        }*/
+
+        // Movimiento a la derecha / izquierda / horizontal_0
+        GetComponent<Rigidbody2D>().velocity = new Vector2(playerSpeed * Input.GetAxisRaw("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+
+        // Girar el Sprite
+        if (Input.GetAxisRaw("Horizontal") == 1) GetComponent<SpriteRenderer>().flipX = false;
+        if (Input.GetAxisRaw("Horizontal") == -1) GetComponent<SpriteRenderer>().flipX = true;
+
+        // Salto solo si no está en el aire
+        if (Input.GetButtonDown("Fire1") && !isJumping) {
             GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
+
+        // Dibujamos la linea
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - offset);
+        Vector2 target = new Vector2(transform.position.x, transform.position.y - offset - lineLength);
+        Debug.DrawLine(origin, target, Color.black);
+
+        // Raycast para detectar colisiones (origen, hacia dónde apunta, largo)
+        RaycastHit2D raycast = Physics2D.Raycast(origin, Vector2.down, lineLength);
+
+        // Detectamos colisiones con el Raycast
+        // Si deberia colisionar pero no lo hace, cambiar el Composite Collider 2D -> Geometry Type --> Polygons
+        if (raycast.collider == null) isJumping = true;
+        else isJumping = false;
     }
 }
